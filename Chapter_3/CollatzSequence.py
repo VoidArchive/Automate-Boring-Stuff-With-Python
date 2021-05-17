0f9a548ed93a1649503709eaca8710bb05abfc2f
Anish Shrestha <voidarchived@gmail.com>
2021-05-17 09:07:20 +0545

    collatz modified

--- Chapter_3/CollatzSequence.py
+++ Chapter_3/CollatzSequence.py
@@ -1,8 +1,11 @@
+
 def collatz(number):
+    # Even number
     if (number % 2) == 0:
         result = number // 2
         print(result)
         return result
+    # Odd number
     else:
         result = 3 * number + 1
         print(result)
@@ -16,5 +19,6 @@ try:
 except ValueError:
     print("Type an integer")
 
+# Loop until input is 1
 while user_input != 1:
     user_input = collatz(user_input)
