25417a67660fda83e9f1f0bc1228bd1e4e338cfd
Anish Shrestha <voidarchived@gmail.com>
2021-05-29 19:28:18 +0545

    strong

---
+++ Chapter_7/strongPassword.py
@@ -0,0 +1,31 @@
+import re
+
+PASSWORD_CHECKS = [
+    re.compile(r'[A-Z]'),
+    re.compile(r'.{8,}'),
+    re.compile(r'[a-z]'),
+    re.compile(r'[0-9]'),
+]
+
+
+def strong_password(password):
+    """
+    Validate if passed password is considered "strong",
+    Password is considered strong if:
+      - is eight characters or longer
+      - contains uppercase and lowercase characters
+      - has one digit or more
+    """
+    return all(check.search(password) for check in PASSWORD_CHECKS)
+
+
+keepgoing = True
+
+while keepgoing:
+    passwordInput = input('Enter a strong password: ')
+    checked = strong_password(passwordInput)
+    if checked == True:
+        print("This is a strong password")
+        keepgoing = False
+    else:
+        print("Weak Weak Enter again")
